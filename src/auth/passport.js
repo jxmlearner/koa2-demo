ddf5603f5042a30411282b0ae42bbcda8008b89e
jxmlearner <410958040@qq.com>
2018-09-12 18:19:34 +0800

    koa-passport使用

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -1,28 +1,40 @@
 //定义策略
 const passport = require('koa-passport')
-var LocalStrategy = require('passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy
+const User=require('../db/schemas/user')
 
 
 // 序列化ctx.login()触发
 passport.serializeUser(function(user, done) {
   console.log('serializeUser: ', user)
-  done(null, user.id)
+  done(null, user)
 })
 // 反序列化（请求时，session中存在"passport":{"user":"1"}触发）
-passport.deserializeUser(async function(id, done) {
-  console.log('deserializeUser: ', id)
-  var user = {id: 1, username: 'admin', password: '123456'}
-  done(null, user)
+passport.deserializeUser(async function(user, done) {
+  console.log('deserializeUser: ', user)
+  await User.findOne({username:user.username,password:user.password}).then(function(data){
+    if(data){
+      console.log('deseralize数据库返回数据',data);
+      done(null,data)
+    }else{
+      done(null,false)
+    }
+  })
 })
 // 提交数据(策略)
 passport.use(new LocalStrategy({
-  // usernameField: 'email',
-  // passwordField: 'passwd'
-}, function(username, password, done) {
-  console.log('LocalStrategy', username, password)
-  var user = {id: 1, username: username, password: password}
-  done(null, user, {msg: 'this is a test'})
-  // done(err, user, info)
+   usernameField: 'username',
+   passwordField: 'userpwd'
+}, async function(username, password, done) {
+  console.log('passport:',username,password)
+  await User.findOne({username,password}).then(function(user){
+    if(user){
+      console.log('验证用户名和密码：',user)
+      done(null,user);
+    }else{
+      done(null,false);
+    }
+  })
 }))
 
 
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,5 @@
 const service=require('../services/user')
+const passport=require('../auth/passport')
 
 module.exports={
   /*
@@ -16,15 +17,42 @@ module.exports={
       data:data?data:[]
     };
   },
-  getUserByNameAndPass:async(ctx,next)=>{
-    let {username,userpwd:password}=ctx.request.body;  //获取post中的数据
-
-    let data=await service.getUserByNameAndPass(username,password);
-    console.log('service返回的用户数据',data);
-    ctx.response.body={
-      status:data?200:100,
-      msg:data?'':'用户名或密码错误',
-      data:data?data:[]
-    };
+  getUserByNameAndPass:async(ctx)=>{
+    // return passport.authenticate('local',function(err,user,info,status){
+    //   if(user===false){
+    //     ctx.body={
+    //       status:100,
+    //       msg:'用户名或密码错误'
+    //     }
+    //   }else{
+    //     ctx.body={
+    //       status:200,
+    //       msg:'登录成功'
+    //     }
+    //     return ctx.login(user)
+    //   }
+    //   // let data=await service.getUserByNameAndPass(username,password);
+    //   // console.log('service返回的用户数据',data);
+    //   // ctx.response.body={
+    //   //   status:data?200:100,
+    //   //   msg:data?'':'用户名或密码错误',
+    //   //   data:data?data:[]
+    //   // };
+    // })(ctx)    
+    // 会调用策略
+    return passport.authenticate('local',function(err, user, info, status) {
+      if(user===false){
+        ctx.body={
+          status:100,
+          msg:'用户名或密码错误'
+        }
+      }else{
+        ctx.body={
+          status:200,
+          msg:'登录成功'
+        }
+        return ctx.login(user)
+      }
+    })(ctx)
   }
 }
